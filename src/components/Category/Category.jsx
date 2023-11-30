fcf41e46e676d244e6b95676996b997a47604b94
Harsh-Makwana <harshmakwana686@gmail.com>
2023-11-30 11:30:34 -0500

    Design Button and ProductCard

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -5,13 +5,13 @@ import './Category.css';
 const Category = (props)=> {
 
     return(
-   <div className="shop">
+   <div className="category-container">
    <h2 className="title">Welcome to shopX</h2>
        <div className="category">
-        <button>Men's Clothing</button>
-        <button>Women's Clothing</button>
-        <button>Electronics</button>
-        <button>Jewelery</button>
+        <button className="category-btn">Men's Clothing</button>
+        <button className="category-btn">Women's Clothing</button>
+        <button className="category-btn">Electronics</button>
+        <button className="category-btn">Jewelery</button>
        </div>
    </div>
     )
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Product.css';
 
 const Product = ({productsData}) => {
 
@@ -14,13 +15,15 @@ const Product = ({productsData}) => {
       {data.map((data) => (
         <div key={data.id} className="productDiv">
           <img src={data.image} />
-          <p className='para'><b>{truncateTitle(data.title, 20)}</b></p>
+          <p className='para'><b>{truncateTitle(data.title, 19)}</b></p>
           <p className='para'><b>$ {data.price}</b></p>
 
           <div className='ratingCount'>
           <span className='para'><b>Rating</b> {data.rating.rate}</span>
           <span className='para'><b>Left</b> {data.rating.count}</span>
           </div>
+
+          <div><button className="addToCart">Add to Cart</button></div>
         </div>
       ))}
     </div>
